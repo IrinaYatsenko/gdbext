5450bb17fa6997628a2267977c8939af91c9eadb
Irina Yatsenko <irinayat@microsoft.com>
2018-09-21 13:31:27 -0700

    Add filtering at the top level of fields in 'dt' command

--- types.py
+++ types.py
@@ -1,4 +1,6 @@
 # Extensions for gdb that dump types
+import gdb
+import re
 
 def print_with_lead(val, lead, spaces_per_lead = 2):
     print(" " * lead * spaces_per_lead + str(val))
@@ -35,11 +37,10 @@ def get_deref_value(var):
     return deref
 
 
-def dt_value(var, print_type, depth, depth_limit):
-    ''' recursively print out offsets, names, values and types of fields in var '''
+def dt_value(var, depth, filter, depth_limit, print_type_name):
     #print('DEBUG: ' + str(var.type.name) + ' at ' + str(depth))
     
-    if print_type:
+    if print_type_name:
         print_with_lead(get_type(var) + ' at ' + str(var.address), depth)
 
     if is_pointer(var.type):
@@ -51,7 +52,7 @@ def dt_value(var, print_type, depth, depth_limit):
                 deref = deref.dereference()
             
             print_with_lead(seq, depth)
-            dt_value(deref, False, depth, depth_limit)
+            dt_value(deref, depth, filter, depth_limit, print_type_name = False)
         except Exception as e:
             #print('DEBUG: ' + str(e))
             pass
@@ -60,17 +61,41 @@ def dt_value(var, print_type, depth, depth_limit):
             # static fields and enums don't affect layout of the type and don't have
             # 'bitpos' property, so let's skip them for now (todo: output them separately?)
             # todo: dump info about base class
-            fields = [(f.bitpos, f.name, var[f.name]) for f in var.type.fields()
-                        if not f.is_base_class and hasattr(f, 'bitpos')]
-        for f in fields:
-            print_with_lead('['+hex(f[0])+']'+' '+f[1]+' : '+str(f[2])[:32]+'...', depth)
-            if depth < depth_limit:
-                dt_value(f[2], True, depth + 1, depth_limit)
+            interesting_fields = [(f.bitpos, f.name, var[f.name]) for f in var.type.fields()
+                        if not f.is_base_class and hasattr(f, 'bitpos') 
+                            and (depth > 0 or filter.match(f.name))]
+            for f in interesting_fields:
+                print_with_lead('['+hex(f[0])+']'+' '+f[1]+' : '+str(f[2])[:32]+'...', depth)
+                if depth < depth_limit:
+                    dt_value(f[2], depth + 1, filter, depth_limit, print_type_name = True)
 
 
-def dt(var_name, depth_limit = 1):
+def dt(var_name, depth_limit = 1, filter = ''):
+    ''' Recursively print out offsets, names, values and types of fields in var_name,
+        which can be name of any variable in the current frame.
+
+        depth_limit: limits recursion into the fields
+        filter: regex string to show only matching fields at the top level
+    '''
     frame = gdb.selected_frame()
     var = frame.read_var(var_name)
-    dt_value(var, print_type = True, depth = 0, depth_limit = depth_limit)
+    dt_value(
+        var, 
+        depth = 0, 
+        filter = re.compile(filter),
+        depth_limit = depth_limit, 
+        print_type_name = True)
 
+def dtv(val, depth_limit = 1, filter = ''):
+    ''' Recursively print out offsets, names, values and types of fields in val,
+        which must be a gdb.Value type.
 
+        depth_limit: limits recursion into the fields
+        filter: regex string to show only matching fields at the top level
+    '''
+    dt_value(
+        val, 
+        depth = 0, 
+        filter = re.compile(filter),
+        depth_limit = depth_limit, 
+        print_type_name = True)
